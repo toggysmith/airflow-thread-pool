b84cf55e1b33e73959a91913617073d256370e96
Toggy Smith <toggysmith@gmail.com>
2023-12-18 13:15:01 +0000

    Set up examples directory

---
+++ examples/example-1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Example 1\n";
+
+	return 0;
+}
---
+++ examples/example-2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Example 2\n";
+
+	return 0;
+}
