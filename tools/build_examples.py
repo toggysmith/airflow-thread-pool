a66ebc2b6a760c76e4e2fe65e53a6df9fbb767ac
Toggy Smith <toggysmith@gmail.com>
2023-12-19 18:39:50 +0000

    Add script for building examples

---
+++ tools/build_examples.py
@@ -0,0 +1,19 @@
+# This script assumes that 'cmake' is available in PATH.
+
+import subprocess
+import os
+
+python_script_location = os.path.dirname(os.path.realpath(__file__))
+os.chdir(python_script_location + "/..")
+
+try:
+    _ = subprocess.run(["cmake", "-B", "build", "-DBUILD_EXAMPLES=ON"]).check_returncode()
+except subprocess.CalledProcessError:
+    print("Error: Attempt to generate build files failed. Exiting early.")
+    exit()
+
+try:
+    _ = subprocess.run(["cmake", "--build", "build"]).check_returncode()
+except subprocess.CalledProcessError:
+    print("Error: Attempt to build using build files failed. Exiting early.")
+    exit()
